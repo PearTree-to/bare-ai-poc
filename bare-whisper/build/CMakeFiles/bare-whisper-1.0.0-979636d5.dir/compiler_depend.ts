ab5f92291cc6b801b177d27b70a598cf0141f130
Ankush SS <ankushss41096@gmail.com>
2024-03-05 00:10:06 +0530

    Remove build

--- bare-whisper/build/CMakeFiles/bare-whisper-1.0.0-979636d5.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for bare-whisper-1.0.0-979636d5.
--- bare-whisper/build/CMakeFiles/bare-whisper-1.0.0-979636d5.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bare-whisper-1.0.0-979636d5.
-# This may be replaced when dependencies are built.
