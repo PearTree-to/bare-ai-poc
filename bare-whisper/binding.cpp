043060bbfc04e137499d74283f158cb888909a3e
agayen <abhijit@peartree.to>
2024-03-09 09:07:07 +0530

    cpp function call

--- bare-whisper/binding.cpp
+++ bare-whisper/binding.cpp
@@ -3,10 +3,15 @@
 #include <js.h>
 #include <stdlib.h>
 
-static js_value_t *
-sum (js_env_t *env, js_callback_info_t *info) {
-  int err;
+// Function to perform addition
+static double add(double left, double right) {
+  return left + right;
+}
 
+static js_value_t *
+bare_sum(js_env_t *env, js_callback_info_t *info) {
+  int err;
+  
   size_t argc = 2;
   js_value_t *argv[2];
 
@@ -16,25 +21,28 @@ sum (js_env_t *env, js_callback_info_t *info) {
   assert(argc == 2);
   
   double left;
-  err = js_get_value_double (env, argv[0], &left);
+  err = js_get_value_double(env, argv[0], &left);
   assert(err == 0);
   
   double right;
-  err = js_get_value_double (env, argv[1], &right);
+  err = js_get_value_double(env, argv[1], &right);
   assert(err == 0);
   
+  // Use the standalone add function
+  double resultValue = add(left, right);
+
   js_value_t *result;
-  err = js_create_double (env, left+right, &result);
+  err = js_create_double(env, resultValue, &result);
   assert(err == 0);
 
   return result;
 }
 
 static js_value_t *
-init (js_env_t *env, js_value_t *exports) {
+init(js_env_t *env, js_value_t *exports) {
   {
     js_value_t *fn;
-    js_create_function(env, "sum", -1, sum, NULL, &fn);
+    js_create_function(env, "sum", -1, bare_sum, NULL, &fn);
     js_set_named_property(env, exports, "sum", fn);
   }
   
