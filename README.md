27e436cad999f5614e6a5eb54d4f4844a6b420e5
agayen <abhijit@peartree.to>
2024-03-19 17:31:04 +0530

    process test

---
+++ README.md
@@ -0,0 +1,16 @@
+Install all dependency libraries.
+
+```
+npm i
+```
+
+for bare
+
+```
+bare index.js
+```
+
+for node
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,5 @@
+const platform = process.platform;
+const arch = process.arch;
+const version = process.version
+
+console.log(platform, arch, version);
