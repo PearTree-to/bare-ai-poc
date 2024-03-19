c39a591b4e85248e31c3fea5f1faf86d874b26d7
agayen <abhijit@peartree.to>
2024-03-19 10:24:55 +0530

    init project set

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Install all dependency  sub git modules
 git submodule update --init --recursive
 ```
 
+Create Bare Addon
+```
+cd whisper-tiny-bare
+```
+```
+bare-dev install
+```
+
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const addon = require("./whisper-tiny-bare/binding");
+const path = require('path');
+const fs = require('fs');
+const { createTranslator, generateText} = require("./whisper-tiny-bare/binding");
 
-console.log(addon)
+const rootDirectory = './'; // if you change the root directory add this
+const usingForTVM = 'metal'; // change this as you use this in your system
+const directoryPath = 'data/audio_bin'; // change the bin file path
+
+const modelDir = path.join(rootDirectory, 'dist/libs/whisper-tiny');
+const sharedObjectPath = path.join(modelDir, 'whisper-tiny.so');
+
+const translator = createTranslator(modelDir, sharedObjectPath, usingForTVM);
+
+// fs.readdir(directoryPath, (err, files) => {
+//   if (err) {
+//     console.error('Error reading directory:', err);
+//     return;
+//   }
+  
+//   const filteredFiles = files.filter(file => file.endsWith('.bin'));
+//   filteredFiles.forEach(filePath => {
+//     const startTime = new Date();
+//     const generatedText = generateText(translator, path.join(rootDirectory,`${directoryPath}/${filePath}`));
+//     const endTime = new Date();
+//     console.log('\nFile Name :', filePath, '\nTranscript :', generatedText, '\nTime :', `${(endTime - startTime)/1000} S`)
+//   });
+// });
